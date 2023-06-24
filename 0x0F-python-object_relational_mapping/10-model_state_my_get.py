bfc902245db225c6187846212638da353a77fb91
Younghope09 <agboedehope4@gmail.com>
2023-06-24 14:56:22 -0700

    Files Updated

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-'''
-Prints the first state object with the name passed as an argumenrt from the databse hbtn_0e_6_usa
-'''
-
+"""
+This script prints the State object id
+with the name passed as argument
+from the database `hbtn_0e_6_usa`.
+"""
 
 from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3],\
-pool_pre_ping=True))
-    state_name = argv[4]
-    flag = 0
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-    
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
+
     session = Session()
+    instance = session.query(State).filter(State.name == argv[4]).first()
 
-    first_state = session.query(State).order_by(State.id)
-
-    for i in first_state:
-        if i.name == state_name:
-            print('{}'.format(i.id))
-            flag = 1
-    if not flag:
+    if instance is None:
         print('Not found')
-
-    session.close()
+    else:
+        print('{0}'.format(instance.id))
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-'''
-deletes of a state object that starts with nletter a
-'''
-
+"""
+This script prints all City objects
+from the database `hbtn_0e_14_usa`.
+"""
 
 from sys import argv
+from model_state import Base, State
+from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State, Base
-from model_city import City
-
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(argv[0], argv[2], argv[3],
-                               pool_pre_ping=True)
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    query_rows = session.query(City, State).\
-        filter(City.state_id == State.id).all()
+    query = session.query(City, State).join(State)
 
-    for city, state in query_rows:
-        print('{}: ({}) {}'.format(state.name, city.id, city.name))
+    for _c, _s in query.all():
+        print("{}: ({:d}) {}".format(_s.name, _c.id, _c.name))
 
-
+    session.commit()
     session.close()
