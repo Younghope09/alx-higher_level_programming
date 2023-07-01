5729ddb1d6706fd2b8e9ca85b760880b8aed1efd
Younghope09 <agboedehope4@gmail.com>
2023-07-01 13:39:07 -0700

    Updated

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-"""
-Returns an integer: the addition of a and b
-This function checks the  datatype if it is  int or float
-Otherwise, raise error, Cast values too
-"""
+"""Module for add_integer method"""
 
 
 def add_integer(a, b=98):
-    if (type(a) is float):
-        a = int(a)
-    if (type(b) is float):
-        b = int(b)
-    if not ((type(a) is int) or (type(a) is float)):
-        raise TypeError('a must be an integer')
-    if not ((type(b) is int) or (type(b) is float)):
-        raise TypeError('b must be an integer')
-    return (a + b)
+    """Adds two integers.
+
+    Args:
+        a: first int.
+        b: second int, default value is 98.
+
+    Raises:
+        TypeError: if a, b are neither int nor float.
+
+    Returns:
+        sum of a and b.
+    """
+
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    elif type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
+
+    if __name__ == "__main__":
+        import doctest
+        doctest.testfile("tests/0-add_integer.txt")
--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,62 +1,65 @@
 #!/usr/bin/python3
-"""
-Return a multiplying matrix function
+"""Module matrix_mul
+Multiplies two matrices and returns the result.
 """
 
 
 def matrix_mul(m_a, m_b):
-    """
-    function that divides all elements of a matrix.
-    arguments: m_a, m_b
-    returns a multiplied matrix of m_a and m_b
-    """
+    """Return the matrix resulting of
+    the multiplication of m_a and m_b."""
 
-    multiplied_matrix = []
-
-    if not (isinstance(m_a, list)):
+    if type(m_a) is not list:
         raise TypeError("m_a must be a list")
-    if not (isinstance(m_b, list)):
+    if type(m_b) is not list:
         raise TypeError("m_b must be a list")
-    
-    if not all(isinstance(row, list) for row in m_a):
-        raise TypeError("m_a must be a list of lists")
-    if not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_b must be a list of lists")
 
-    if (m_a == [] or m_a == [[]]):
+    for x in m_a:
+        if type(x) is not list:
+            raise TypeError("m_a must be a list of lists")
+    for x in m_b:
+        if type(x) is not list:
+            raise TypeError("m_b must be a list of lists")
+
+    if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
-    if (m_b == [] or m_b == [[]]):
+    if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
-    
-    if not all((isinstance(element, int) or isinstance(element, float))
-               for element in [num for row in m_a for num in row]):
-        raise TypeError("m_a should contain only integers or floats")
-    if not all((isinstance(element, int) or isinstance(element, float))
-               for element in [num for row in m_b for num in row]):
-        raise TypeError("m_b should contain only integers or floats")
-    
-    if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must should be of the same size")
-    if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must should be of the same size")
-    
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
-    
-    inverting_m_b = []
-    for row in range(len(m_b[0])):
-        generated_row = []
-        for column in range(len(m_b)):
-            generated_row.append(m_b[column][row])
-        inverting_m_b.append(generated_row)
 
     for row in m_a:
-        generated_row = []
-        for column in inverting_m_b:
-            multiplication = 0
-            for i in range(len(inverting_m_b[0])):
-                multiplication += row[i] * column[i]
-            generated_row.append(multiplication)
-        multiplied_matrix.append(generated_row)
+        for x in row:
+            if type(x) is not int and type(x) is not float:
+                raise TypeError("m_a should contain only integers or floats")
+    for row in m_b:
+        for x in row:
+            if type(x) is not int and type(x) is not float:
+                raise TypeError("m_b should contain only integers or floats")
 
-    return multiplied_matrix
+    row_len = []
+    for row in m_a:
+        row_len.append(len(row))
+    if not all(elem == row_len[0] for elem in row_len):
+            raise TypeError("each row of m_a must should be of the same size")
+    row_len = []
+    for row in m_b:
+        row_len.append(len(row))
+    if not all(elem == row_len[0] for elem in row_len):
+            raise TypeError("each row of m_b must should be of the same size")
+
+    a_col = 0
+    for col in m_a[0]:
+        a_col += 1
+    b_row = 0
+    for row in m_b:
+        b_row += 1
+
+    if a_col != b_row:
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    result = [[0 for x in range(len(m_b[0]))] for y in range(len(m_a))]
+
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                result[i][j] += m_a[i][k] * m_b[k][j]
+
+    return result
